9d31df80acb842c0528cd4e297cbcd0c8c02ff1e
sholq <semyon.silkin@gmail.com>
2022-06-09 20:48:03 +0300

    feat: add db, schemes and models

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-// Слушаем 3000 порт
+const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
