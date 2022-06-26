5f1929612fe053b77dc9b059ff4d0a6c41b432c5
sholq <semyon.silkin@gmail.com>
2022-06-26 19:20:53 +0300

    fix: fix joi validation

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
+    name: Joi.string().min(8).max(30),
+    about: Joi.string().min(8).max(30),
+    avatar: Joi.string(),
   })
 }), createUser);
 app.use(auth);
--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ usersRouter.get('/me', getProfile);
 usersRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2),
+    about: Joi.string().required().min(2).max(30),
   })
 }), updateProfile);
 
