57f37f67e7d0b7528072ed973b5351625599f14b
sholq <semyon.silkin@gmail.com>
2022-06-26 21:00:07 +0300

    fix: change cookie on body into login responce

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,13 +41,7 @@ module.exports.login = (req, res, next) => {
         { expiresIn: '7d' },
       );
 
-      res
-        .cookie('jwt', token, {
-          maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
-          sameSite: true,
-        })
-        .send({ message: 'Вы вошли!' });
+      res.send({ token });
     })
     .catch((err) => {
       next(new AuthError(err.message));
