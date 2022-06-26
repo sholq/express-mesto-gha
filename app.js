ffd4784899031e30bbb22329acfdc05fdab06a3a
sholq <semyon.silkin@gmail.com>
2022-06-26 19:25:33 +0300

    fix: fix minlength of user data

--- app.js
+++ app.js
@@ -50,8 +50,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
-    name: Joi.string().min(8).max(30),
-    about: Joi.string().min(8).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string(),
   })
 }), createUser);
